d3730b03405750efe523d96d23f45cd54ecb91e2
Archu-1234 <ashwiniarchana399@gmail.com>
2024-04-01 19:03:20 +0530

    text

---
+++ LinkedlistOne.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+class test{
+    public static void main(string[] args){
+        linked List<Integer> ids=new LinkedList<Integer>();
+        System.out.println(ids);
+        ids.add(e;101);
+        ids.add(e:102);
+
+        Iterator itr=ids.iterator();
+        ids.addfirst(e:101);
+        ids.addLast(e:102);
+
+
+    }
+}
